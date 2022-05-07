f75b9708e840fd8b18e5b4616ef1236843ab5fd4
Sergei <alp-rostov@mail.ru>
2022-05-07 19:04:48 +0300

    add file sea_battle.py

--- skill-projects/sea_battle.py
+++ skill-projects/sea_battle.py
@@ -1,6 +1,6 @@
-
+#игра морской бой
 class PlayingField(): # класс иговых полей
-    def __init__(self, A):
+    def __init__(self, A:list):
         self.A=A
 
     def ShowField(self):
@@ -10,9 +10,39 @@ class PlayingField(): # класс иговых полей
             print(str(count)+'  '+" | ".join(i))
             count+=1
 
+class Ship(): #класс кораблей
+    def __init__(self, S:list):
+        self.S=S #спискок координат
 
-B=[['0' for i in range(6)] for i in range(6)]
+    @property
+    def side_(self):
+        return self.S
 
-Field=PlayingField(B)
+    @side_.setter
+    def side_(self,value):
+        self.S =value
 
-Field.ShowField()
+
+
+
+
+
+d1=Ship([[1,1],[1,2]])
+d2=Ship([[6,6]])
+
+
+
+print(d1.S[1][1])
+
+
+D=[['0' for i in range(6)] for i in range(6)]
+
+for i in d1.S:
+    D[i[0]-1][i[1]-1]='x'
+for j in d2.S:
+    D[j[0]-1][j[1]-1]='x'
+
+
+D_Field=PlayingField(D)
+
+D_Field.ShowField()
