3f8bb415982018a315f42c526cf77846f6fb57b5
Sergei <alp-rostov@mail.ru>
2022-07-13 23:14:26 +0300

    upd

--- NewsPortal1/newsWeb/news/models.py
+++ NewsPortal1/newsWeb/news/models.py
@@ -15,22 +15,45 @@ class Comments(models.Model): #Table of comments for the post
     text=models.TextField() # text comment
     date=models.DateTimeField(auto_now_add = True) #date of the comment will be added
     sum_rank=models.IntegerField(default=0) #rank of comment
-    count_rank=models.IntegerField(default=0) #count of vote
     id_users=models.ForeignKey('Users', on_delete=models.CASCADE) # onetomany relation to the table "Users"
 
+    def like(self):
+        self.sum_rank=+1
+        self.save()
+
+    def dislike(self):
+        if self.sum_rank>0:
+            self.sum_rank=-1
+            self.save()
+
 POSITIONS = [('news', 'Новости'), ('article', 'Статьи')]
+
 class Post(models.Model): #Table for the posts
     head_article=models.CharField(max_length=2550) #post`s name
     text_post=models.TextField() #text
     date=models.DateTimeField(auto_now_add = True) #date of the post will be added
     type_post=models.CharField(max_length = 20, choices = POSITIONS, default = 'news') #post`s type : news or article
     sum_rank = models.IntegerField ( default=0 )
-    count_rank = models.IntegerField ( )
     id_author=models.ForeignKey('Author', on_delete=models.CASCADE) # onetomany relation to the table "Author"
     id_category=models.ManyToManyField('Category', through=PostCategory) #manytomany relation to the table "Category"
+
+    def like(self):
+        self.sum_rank = +1
+        self.save ( )
+
+    def dislike(self):
+        if self.sum_rank > 0:
+            self.sum_rank = -1
+            self.save ( )
+
+    def preview(self):
+        return str(self.text_post)[0:124]
 
 class Author(models.Model): #Table for the author
     full_name=models.CharField(max_length=255)
     e_mail=models.EmailField(max_length=150, null=True)
     rank=models.IntegerField(default=0)
     id_users=models.OneToOneField('Users', on_delete=models.CASCADE) # onetomany relation to the table "Users"
+
+    def update_rating (self):
+        pass
