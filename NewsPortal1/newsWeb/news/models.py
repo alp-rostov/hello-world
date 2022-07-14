dcb80775dd029ce2fede19990cb2f2fe780f5e6d
Sergei <alp-rostov@mail.ru>
2022-07-14 23:21:38 +0300

    xxx

--- NewsPortal1/newsWeb/news/models.py
+++ NewsPortal1/newsWeb/news/models.py
@@ -1,18 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Category(models.Model): #Table of categories
     name=models.CharField(max_length=255, unique=True)
 
 class PostCategory(models.Model): #Table for manytomany relation Post and Category
     id_post=models.ForeignKey('Post', on_delete=models.CASCADE)
-    id_caregory=models.ForeignKey('Category', on_delete=models.CASCADE)
+    id_category=models.ForeignKey('Category', on_delete=models.CASCADE)
 
 class Comments(models.Model): #Table of comments for the post
     id_post=models.ForeignKey('Post', on_delete=models.CASCADE) # onetomany relation to the table "Post"
     text=models.TextField() # text comment
     date=models.DateTimeField(auto_now_add = True) #date of the comment will be added
     sum_rank=models.IntegerField(default=0) #rank of comment
-    id_users=models.IntegerField()
+    id_users=models.OneToOneField(User, on_delete=models.CASCADE)
 
     def like(self):
         self.sum_rank=+1
@@ -32,7 +33,8 @@ class Post(models.Model): #Table for the posts
     type_post=models.CharField(max_length = 20, choices = POSITIONS, default = 'news') #post`s type : news or article
     sum_rank = models.IntegerField ( default=0 )
     id_author=models.ForeignKey('Author', on_delete=models.CASCADE) # onetomany relation to the table "Author"
-    id_category=models.ManyToManyField('Category', through=PostCategory) #manytomany relation to the table "Category"
+
+    category=models.ManyToManyField('Category', through=PostCategory) #manytomany relation to the table "Category"
 
     def like(self):
         self.sum_rank = +1
@@ -44,13 +46,13 @@ class Post(models.Model): #Table for the posts
             self.save ( )
 
     def preview(self):
-        return str(self.text_post)[0:124]
+        return self.text_post[0:124]
 
 class Author(models.Model): #Table for the author
     full_name=models.CharField(max_length=255)
     e_mail=models.EmailField(max_length=150, null=True)
     rank=models.IntegerField(default=0)
-    id_users=models.IntegerField()
+    id_users=models.OneToOneField(User, on_delete=models.CASCADE)
 
     def update_rating (self):
         pass
